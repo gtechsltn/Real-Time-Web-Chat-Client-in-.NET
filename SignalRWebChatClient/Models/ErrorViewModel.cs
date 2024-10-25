2fa9ae4839ba700e1d2ea9b46598cabf6beb85f7
MD HASAN MONSUR <hasanmcse@gmail.com>
2024-10-26 00:55:47 +0600

    hasan

---
+++ SignalRWebChatClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SignalRWebChatClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
